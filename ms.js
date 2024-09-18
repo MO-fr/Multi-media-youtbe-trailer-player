97cdf541946950f4f547c28fb2d6a4e09a1fe38f
Mohamed <msoua0040@launchpadphilly.org>
2024-09-17 22:09:17 -0400

    ms1 update

---
+++ ms.js
@@ -0,0 +1,33 @@
+const videos = [
+    'https://www.youtube.com/embed/KAE5ymVLmZg?si=oimcrLGCMy4Z0Dgk', 
+    'https://www.youtube.com/embed/aV8H7kszXqo?si=ojDapESyEZmhIH83',
+    'https://www.youtube.com/embed/RlOB3UALvrQ?si=M7BNO-PZa7eHXzsd',
+    'https://www.youtube.com/embed/v7MGUNV8MxU?si=_LFd8ZzVKii-y0h-',
+    'https://www.youtube.com/embed/6ZfuNTqbHE8?si=yvg9jksJYWVKCIOp',
+    'https://www.youtube.com/embed/tmeOjFno6Do?si=VdzcdHSerhqxefW7',
+    'https://www.youtube.com/embed/dKrVegVI0Us?si=E6Wqql47BE4WmMBx',
+    'https://www.youtube.com/embed/rt-2cxAiPJk?si=StNOAYdNMUDIRVxe',
+    'https://www.youtube.com/embed/TcMBFSGVi1c?si=ll7a4Ur-LbD9YtIQ'
+];
+
+let currentVideoIndex = 0;
+
+// Select the iframe using the tag name
+const videoPlayer = document.querySelector("iframe");
+
+function updateVideo() {
+    videoPlayer.src = videos[currentVideoIndex];
+}
+
+function nextVideo() {
+    currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+    updateVideo();
+}
+
+function prevVideo() {
+    currentVideoIndex = (currentVideoIndex - 1 + videos.length) % videos.length;
+    updateVideo();
+}
+
+document.querySelector(".next-button").addEventListener("click", nextVideo);
+document.querySelector(".back-button").addEventListener("click", prevVideo);
