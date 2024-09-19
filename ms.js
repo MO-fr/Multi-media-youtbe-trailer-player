8adc1bee0764179a1c6729e41538f3c510594231
Mohamed <msoua0040@launchpadphilly.org>
2024-09-19 11:56:43 -0400

    ms update

--- ms.js
+++ ms.js
@@ -10,17 +10,41 @@ const videos = [
     'https://www.youtube.com/embed/TcMBFSGVi1c?si=ll7a4Ur-LbD9YtIQ'
 ];
 
+
 let currentVideoIndex = 0;
 
 // Select the iframe using the tag name
-const videoPlayer = document.querySelector("iframe");
+var iframe = document.createElement('iframe');
+
+        // Set attributes for the iframe
+        iframe.src = "https://www.youtube.com/embed/KAE5ymVLmZg?si=oimcrLGCMy4Z0Dgk";
+        iframe.width = "560px";
+        iframe.height = "315px";
+        iframe.title = "YouTube video player";
+
+        
+        // Optionally set styles
+        iframe.style.border = "2px solid black";
+
+        // Append the iframe to a container in the DOM
+        let div = document.getElementById('vid-container')
+        div.appendChild(iframe);
+     
+console.log("videoPlayer Id ", videoPlayer)
 
 function updateVideo() {
-    videoPlayer.src = videos[currentVideoIndex];
+    console.log("videoPlayer ", videoPlayer)
+
+    
+
+    // videoPlayer.src = videos[currentVideoIndex];
 }
 
 function nextVideo() {
-    currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+    
+    console.log("test");
+    currentVideoIndex += 1 % videos.length;
+    console.log(currentVideoIndex)
     updateVideo();
 }
 
@@ -29,5 +53,5 @@ function prevVideo() {
     updateVideo();
 }
 
-document.querySelector(".next-button").addEventListener("click", nextVideo);
+document.querySelector("next-button").addEventListener("click", nextVideo);
 document.querySelector(".back-button").addEventListener("click", prevVideo);
